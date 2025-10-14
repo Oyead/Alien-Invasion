b5e47fb004d90c64c06112e2a10a6675c94f9d0f
Eyad <eyadswailam12345@gmail.com>
2025-10-14 18:27:21 +0300

    add ship_speed attribute

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ class Settings:
         self.screen_width = 1500
         self.screen_height = 720
         self.bg_color = (230,230,230)
-
+     #Ship Settings
+        self.ship_speed=0.5
 
          

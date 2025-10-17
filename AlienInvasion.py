face41cb613be3c3e4a4c001e01380d7a1bcbf1e
Eyad <eyadswailam12345@gmail.com>
2025-10-17 20:13:34 +0300

    implement ship count in main game

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -69,6 +69,7 @@ class AlienInvasion:
           self.stats.game_active = True
           self.sb.prep_score()
           self.sb.prep_level()
+          self.sb.prep_ships()
           # Hide the mouse cursor.
           pygame.mouse.set_visible(False)
           # Get rid of any remaining aliens and bullets.
@@ -191,8 +192,9 @@ class AlienInvasion:
     def _ship_hit(self):
        """Respond to the ship being hit by an alien."""
        if self.stats.ship_left > 0:
-         # Decrement ships_left.
+         # Decrement ships_left, and update scoreboard.
          self.stats.ship_left -= 1
+         self.sb.prep_ships()
          # Get rid of any remaining aliens and bullets.
          self.aliens.empty()
          self.bullets.empty()

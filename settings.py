aaa7b36401518bb6192d549b6764d37e6f2d4aa1
Eyad <eyadswailam12345@gmail.com>
2025-10-17 15:54:14 +0300

    change bullet speed

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings:
      #Ship Settings
         self.ship_speed=0.75
     #Bullets Settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
